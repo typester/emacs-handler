ce1b5fa8c8f681b7de0e7c3111538308f88b3356
Daisuke Murase <typester@cpan.org>
2009-10-30 19:26:42 +0900

    template

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  emacs-handler
+//
+//  Created by Daisuke Murase on 09/10/30.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
