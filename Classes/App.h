b337d4c1662e84421ed9b2bf1172727a0d8c22ca
Daisuke Murase <typester@cpan.org>
2009-10-31 16:22:55 +0900

    add setting panel

--- Classes/App.h
+++ Classes/App.h
@@ -1,7 +1,13 @@
 #import <Cocoa/Cocoa.h>
 
 @interface App : NSObject {
+    NSString *path;
 
+    IBOutlet NSWindow *prefPanel;
+    IBOutlet NSTextField *textField;
 }
+
+-(IBAction)showPrefPanel:(id)sender;
+-(IBAction)applyChange:(id)sender;
 
 @end
--- Classes/App.m
+++ Classes/App.m
@@ -5,11 +5,16 @@
 @implementation App
 
 -(void)awakeFromNib {
+    NSUserDefaults *d = [NSUserDefaults standardUserDefaults];
+    path = [d objectForKey:@"path"];
+
     NSAppleEventManager *appleEventManager = [NSAppleEventManager sharedAppleEventManager];
     [appleEventManager setEventHandler:self andSelector:@selector(handleGetURLEvent:withReplyEvent:) forEventClass:kInternetEventClass andEventID:kAEGetURL];
 }
 
 -(void)handleGetURLEvent:(NSAppleEventDescriptor *)event withReplyEvent:(NSAppleEventDescriptor *)replyEvent {
+    if (nil == path) return;
+
     // emacs://open/?url=file://~/.bash_profile&line=11&column=2
     NSURL *url = [NSURL URLWithString:[[event paramDescriptorForKeyword:keyDirectObject] stringValue]];
 
@@ -24,13 +29,34 @@
 
             if (file) {
                 NSTask *task = [[NSTask alloc] init];
-                [task setLaunchPath:@"/Applications/CarbonEmacs.app/Contents/MacOS/bin/emacsclient"];
+                [task setLaunchPath:path];
                 [task setArguments:[NSArray arrayWithObjects:@"-n", [NSString stringWithFormat:@"+%d:%d", [line integerValue], [column integerValue]], file, nil]];
                 [task launch];
                 [task release];
             }
         }
     }
+}
+
+-(IBAction)showPrefPanel:(id)sender {
+    if (path) {
+        [textField setStringValue:path];
+    }
+    else {
+        [textField setStringValue:@""];
+    }
+    [prefPanel makeKeyAndOrderFront:nil];
+}
+
+-(IBAction)applyChange:(id)sender {
+    path = [textField stringValue];
+
+    if (path) {
+        NSUserDefaults *d = [NSUserDefaults standardUserDefaults];
+        [d setObject:path forKey:@"path"];
+    }
+
+    [prefPanel orderOut:nil];
 }
 
 @end
